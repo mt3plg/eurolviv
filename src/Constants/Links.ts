2898d150fabc797ec9cbb75ffcaa53a4fb9889d2
mt3plg <vladtzm@gmail.com>
2025-11-05 11:14:53 +0200

    Update restaurant and terrace booking link to expz.app/ROOF

--- src/Constants/Links.ts
+++ src/Constants/Links.ts
@@ -1,6 +1,6 @@
 export const links ={
     menu:"https://expz.menu/65e11efa-1922-4801-9bc2-9ffadd3e1b99?fbclid=PAZXh0bgNhZW0CMTEAAabJL0bpF0EgqIhUP8ERU-rGf8bewkMFncsxz-w0B418Rnd8XLUsh4C4rSQ_aem_1RISpAdCrSheyenFx3LiAA",
-    bookTable:"https://app.takeuseat.in.ua/RUFF?fbclid=PAZXh0bgNhZW0CMTEAAaZ82EnX_97NMVHW7qwkRFGHkVM2cZEujehwNfja2qGZidA3HbiCkdQGD6k_aem_BiLh6EPICbWd07Rg4gWf8g",
+    bookTable:"https://expz.app/ROOF",
     wineCard:'https://expz.menu/65e11efa-1922-4801-9bc2-9ffadd3e1b99/menu?menuId=15499',
     alcoholCard:'https://expz.menu/65e11efa-1922-4801-9bc2-9ffadd3e1b99/menu?menuId=15683',
     cocktailCard:"https://expz.menu/65e11efa-1922-4801-9bc2-9ffadd3e1b99/menu?menuId=15497"
